7d066165c4e067d652c5a0fd3c8d565f7fb7fab0
Lakindu Wickramasinghe <86288879+lakinduwickramasinghe@users.noreply.github.com>
2024-05-22 20:09:44 +0530

    last

--- JS/menu.js
+++ JS/menu.js
@@ -1,20 +1,8 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-}
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times')
-    navbar.classList.remove('active')
-}
-
-document.querySelector('#search-icon').onclick = () =>{
-    document.querySelector('#search-form').classList.toggle('active')
-}
-
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active')
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBars = document.getElementById('menu-bars');
+    const verticalMenu = document.getElementById('vertical-menu');
+    
+    menuBars.addEventListener('click', function() {
+        verticalMenu.classList.toggle('active');
+    });
+});
