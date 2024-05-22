adb77447992e922430553d551e3cd883b9b65db2
Lakindu Wickramasinghe <86288879+lakinduwickramasinghe@users.noreply.github.com>
2024-05-22 23:42:19 +0530

    h

--- JS/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuBars = document.getElementById('menu-bars');
-    const verticalMenu = document.getElementById('vertical-menu');
-    
-    menuBars.addEventListener('click', function() {
-        verticalMenu.classList.toggle('active');
-    });
-});
